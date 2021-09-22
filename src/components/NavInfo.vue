d4ed8ff7824719f56c6295b11b46cd335650fc13
sauriomac <lrodrigoespinoza@gmail.com>
2021-09-23 00:07:44 +0300

    update

--- src/components/NavInfo.vue
+++ src/components/NavInfo.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <!-- <nav id="mn_menu" class="navbar pb-n5 pt-4 navbar-expand-lg navbar-light">
-            <ul class="nav navbar-nav ml-auto mr-5">
-            </ul>
-        </nav> -->
     <nav
       id="mn_menu"
       class="navbar pt-3 pl-5 pb-0 pt-0 ml-auto navbar-expand-lg navbar-light"
@@ -14,26 +10,26 @@
         <h3 class="envio mt-2 ml-3">ДОСТАВКА | С СОБОЙ ПН-ВС С 10 ДО 23</h3>
       </li>
       <li class="nav-item ml-4">
-        <a class="phone" href="https://goo.gl/maps/FapfeyWphA2QeUWdA"
+        <a class="phone" href="#"
           ><span class="material-icons"> place </span>магазину</a
         >
       </li>
 
       <li class="nav-item">
-        <span class="material-icons mr-1"> phone </span>
-        <a class="phone" href="tel:+380685051622">0685051622</a>
+        <span class="material-icons mr-2"> phone </span
+        ><a class="phone" href="#">0930000000</a>
       </li>
       <li class="nav-item">
         <span class="material-icons mr-2"> phone </span
-        ><a class="phone" href="tel:+380933593958">0933593958</a>
+        ><a class="phone" href="#">0930000000</a>
       </li>
       <li class="nav-item">
         <span class="material-icons mr-2"> phone </span
-        ><a class="phone" href="tel:+380662434764">0662434764</a>
+        ><a class="phone" href="#">0660000000</a>
       </li>
       <li class="nav-item ml-2">
         <span class=""
-          ><a class="phone" href="https://www.instagram.com/sushiwowua/"
+          ><a class="phone" href="#"
             ><img class="redes" src="../assets/instagram.svg" /></a
         ></span>
       </li>
@@ -41,19 +37,19 @@
         <span class=""
           ><a
             class="phone"
-            href="https://wa.me/380685051622?text=desde%20suchiwow"
+            href="#"
             ><img class="redes" src="../assets/whatsapp.svg" /></a
         ></span>
       </li>
       <li class="nav-item ml-1">
         <span class=""
-          ><a class="phone" href="viber://chat?​number=380685051622"
+          ><a class="phone" href="#"
             ><img class="redes" src="../assets/viber.svg" /></a
         ></span>
       </li>
       <li class="nav-item m-2">
         <span class=""
-          ><a class="phone" href="https://t.me/380685051622"
+          ><a class="phone" href="#"
             ><img class="redes" src="../assets/telegram.svg" /></a
         ></span>
       </li>
